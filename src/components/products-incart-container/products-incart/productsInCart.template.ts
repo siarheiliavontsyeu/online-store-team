1b851974a0956ba15d5439ab378225ecde8b0122
Liavontsyeu Siarhei <leontevsi@gmail.com>
2023-01-05 00:04:47 +0300

    feat: [os-40] add productsInCart template

--- src/components/products-incart-container/products-incart/productsInCart.template.ts
+++ src/components/products-incart-container/products-incart/productsInCart.template.ts
@@ -1,4 +1,15 @@
 export const getTemplate = (): string => {
   return `
-  ProductsInCart`;
+  <div class="card text-white bg-dark mb-3">
+    <div class="card-header text-warning d-flex justify-content-between">
+    <div><i class="fas fa-boxes"></i> Products In Cart</div>
+    <div class="d-flex">
+      <div class="px-2">Limit: <input type="number" min="1" class="" max="5" value="1"></div>
+      <div class="px-2">Page: <input type="number" min="1" class="" max="5" value="1"></div>
+    </div>
+    </div>
+    <div class="card-body d-flex">
+      <div class="products-incart-container"></div>
+    </div>
+  </div>`;
 };
