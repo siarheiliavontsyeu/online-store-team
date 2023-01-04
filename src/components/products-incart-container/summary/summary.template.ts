212043863a831f43dfeca17e9fcb91e9da8dff62
Liavontsyeu Siarhei <leontevsi@gmail.com>
2023-01-04 23:28:18 +0300

    feat: [os-40] add Summary template

--- src/components/products-incart-container/summary/summary.template.ts
+++ src/components/products-incart-container/summary/summary.template.ts
@@ -1,5 +1,17 @@
 export const getTemplate = (): string => {
   return `
-   Summary
- `;
+  <div class="card text-white bg-dark mb-3" style="max-width: 20rem;">
+    <div class="card-header text-warning"><i class="fas fa-box-check"></i> Summary</div>
+    <div class="card-body d-flex flex-column justify-content-center align-items-center">
+      <p class="text-warning">Products: 45</p>
+      <p class="text-warning">Total: <i class="fas fa-euro-sign"></i>27,286.00</p>
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Enter promo code" id="promo">
+        <p class="text-muted">Promo for test: 'RS', 'EPM'</p>
+      </div>
+      <button id="buy-now" type="button" class="btn btn-danger">
+      <i class="fas fa-shopping-cart"></i> BUY NOW
+      </button>
+    </div>
+  </div>`;
 };
