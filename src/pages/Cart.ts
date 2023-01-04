3ef39b178c657095bbaec15a6bb43a2b64dd020a
Liavontsyeu Siarhei <leontevsi@gmail.com>
2023-01-04 22:13:05 +0300

    feat: [os-40] add CartPageContainer, add navigate to cart

--- src/pages/Cart.ts
+++ src/pages/Cart.ts
@@ -1,16 +1,27 @@
-import { createNode, DomNode } from '../core/components/node.core';
+import { CartPageContainer } from '../components/cart-page/CartPageContainer';
+import { DomNode } from '../core/components/node.core';
+import { CurrentRoute } from '../core/router/currentRoute';
 import { Page } from '../core/router/page';
 import Store from '../core/store/store.core';
 
 export class Cart extends Page {
+  private cartPageContainer: CartPageContainer | null;
+
   constructor(public store: Store) {
     super(store);
+    this.cartPageContainer = null;
   }
 
   render(): DomNode {
-    return createNode({ tag: 'div', classes: ['container', 'cart-page'] }).html(
-      `<h1>Cart Page</h1>
-    <button type="button" class="btn btn-link"><a href="#product">Product</a></button>`
-    ) as DomNode;
+    this.cartPageContainer = new CartPageContainer(this.store);
+    return this.cartPageContainer.render();
+  }
+
+  afterRender() {
+    this.cartPageContainer?.init();
+  }
+
+  destroy() {
+    this.cartPageContainer?.destroy();
   }
 }
