bd5c6d0bf34f73e00a74a42e96859204de44b364
Alina <astapchik.1993@mail.ru>
2023-01-07 01:21:35 +0300

    feat: add changing of the product card by adding to the cart

--- src/components/filters-products-container/products/products-container/productCard/productCard.component.ts
+++ src/components/filters-products-container/products/products-container/productCard/productCard.component.ts
@@ -8,6 +8,8 @@ export default class productCard extends Component {
   static className = 'product-item';
 
   private cardData: ProductI;
+  // private $btnAddtoCart: DomNode | false;
+  // private $productCard: DomNode | false;
   private $btnAddtoCart: DomNode | false;
   private $productCard: DomNode | false;
 
@@ -18,12 +20,16 @@ export default class productCard extends Component {
       listeners: ['click'],
     });
     this.cardData = cardData;
+    // this.$btnAddtoCart = false;
+    // this.$productCard = false;
     this.$btnAddtoCart = false;
     this.$productCard = false;
   }
 
   init() {
     super.init();
+    // this.$productCard = this.$root.find(`#product-card-${this.cardData.id}`);
+    // this.$btnAddtoCart = this.$root.find(`#add-to-cart-btn-${this.cardData.id}`);
     this.$productCard = this.$root.find(`#product-card-${this.cardData.id}`);
     this.$btnAddtoCart = this.$root.find(`#add-to-cart-btn-${this.cardData.id}`);
   }
@@ -32,6 +38,22 @@ export default class productCard extends Component {
     return renderProductCard(this.cardData);
   }
 
+  // onClick(e: Event) {
+  //   this.store.getProductsForView()
+  //   if(this.$btnAddtoCart && this.$productCard) {
+  //     if(this.cardData.isInCart) {
+  //       this.store.dropFromCart(this.cardData.id);
+  //       this.$productCard.replaceClass('in-cart', 'not-in-cart')
+  //       this.$btnAddtoCart.html('Add to cart');
+  //     } else {
+  //       this.store.addToCart(this.cardData.id)
+  //       this.$btnAddtoCart.html('<i class="fas fa-check-double"></i> Drop')
+  //       this.$productCard.replaceClass('not-in-cart', 'in-cart')
+  //     }
+  //   }
+
+  //   console.log(this.cardData.isInCart, this.store.state.cart)
+  // }
   onClick(e: Event) {
     this.store.getProductsForView()
     if(this.$btnAddtoCart && this.$productCard) {
@@ -45,10 +67,7 @@ export default class productCard extends Component {
         this.$productCard.replaceClass('not-in-cart', 'in-cart')
       }
     }
-
-    console.log(this.cardData.isInCart, this.store.state.cart)
   }
-
   destroy() {
     super.destroy();
     this.$root.clear();
