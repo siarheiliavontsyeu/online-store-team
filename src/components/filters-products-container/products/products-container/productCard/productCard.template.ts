83f58510cd60c098b36fe55a888a4e05830dbac9
RemyJS <a.king92@mail.ru>
2023-01-07 12:46:58 +0300

    feat[OS-48]: update card styles

--- src/components/filters-products-container/products/products-container/productCard/productCard.template.ts
+++ src/components/filters-products-container/products/products-container/productCard/productCard.template.ts
@@ -1,24 +1,44 @@
 import { ProductI } from '../../../../../constants/types';
 
+const getRatingStars = (rating: number) => rating < 4.5 
+? '<i class="fas fa-star-half-alt"></i>' 
+: '<i class="fas fa-star"></i>';
+
+
 export const renderProductCard = (card: ProductI) => {
-  return `<div class="product-card card " >
-  
-    <div class="product-wrapper" style="background: url(${card.images[0]}) 0% 0% / cover">
-    <h4 class="product-title card-header bg-primary">${card.title}</h4>
-      <div class="product-info">
-        <div class="product-info__category">Category: ${card.category}</div>
-        <div class="product-info__brand">Brand: ${card.brand}</div>
-        <div class="product-info__price">Price: ${card.price}</div>
-        <div class="product-info__discount">Discount: ${card.discountPercentage}</div>
-        <div class="product-info__rating">Rating: ${card.rating}</div>
-        <div class="product-info__stock">Stock: ${card.stock}</div>
-      </div>
-    </div>
-    <div class="buttons-wrapper">
-    <button type="button" class="btn btn-product btn-warning">Add to cart</button>
-    <a href="#product/${card.id}" class="btn btn-product btn-info">Details</a>
+  return `<div class="card product-card">
+  <h5 class="card-header">${card.brand}</h5>
+  <img src=${card.images[0]} class="card-img-top" alt=${card.title}>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <p class="text-muted mb-0">Available: <span class="fw-bold">${card.stock}</span></p>
+    </li>
+  </ul>
+  <div class="card-body">
+  <div class="d-flex justify-content-between mb-2">
+  <p class="text-muted mb-0">Rating: </span></p>
+  <div class="ms-auto text-warning">
+    <i class="fa fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+      ${getRatingStars(card.rating)}
     </div>
   </div>
+  <div class="d-flex justify-content-between">
+    <p class="small text-muted">${card.category}</p>
+    <p class="small text-danger"><s>${card.price + card.discountPercentage}</s></p>
+  </div>
+  <div class="d-flex justify-content-between mb-3">
+    <h6 class="mb-0">${card.title}</h6>
+    <h6 class="text-light mb-0">$${card.price}</h6>
+  </div>
+  <div class="buttons-wrapper">
+    <button type="button" class="btn btn-product btn-warning">Add to cart</button>
+    <a href="#product/${card.id}" class="btn btn-product btn-info">Details</a>
+  </div>
+  </div>
+</div>
 `
 };
 
