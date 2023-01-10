88a26590b31e6fbe8835d6d72ea4400b8e4ebf07
Alina <astapchik.1993@mail.ru>
2023-01-10 05:29:14 +0300

    feat: add validation in personal details

--- src/components/modalPayment/ModalPayment.component.ts
+++ src/components/modalPayment/ModalPayment.component.ts
@@ -3,12 +3,20 @@ import { DomNode, wrapperNode } from '../../core/components/node.core';
 import { getModalTemplate } from './modalPayment.template';
 import { ComponentOptions } from '../../constants/types';
 import { Actions } from '../../constants/actions';
+import { validAddress, validCard, validCardDate, validCVV, validEmail, validName, validPhone } from '../../utils/validations';
 
 export default class ModalPayment extends Component {
   static tagName = 'div';
   static className = 'modal hidden';
   private $btnSubmit: DomNode | false;
   private $btnClose: DomNode | false;
+  private $inputName: DomNode | false;
+  private $inputNumber: DomNode | false;
+  private $inputAdress: DomNode | false;
+  private $inputEmail: DomNode | false;
+  private $inputCardNumber: DomNode | false;
+  private $inputCardValid: DomNode | false;
+  private $inputCardCVV: DomNode | false;
 
   constructor($root: DomNode, options: ComponentOptions) {
     super($root, {
@@ -18,6 +26,14 @@ export default class ModalPayment extends Component {
     });
     this.$btnSubmit = false;
     this.$btnClose = false;
+    this.$modalForm = false;
+    this.$inputName = false;
+    this.$inputNumber = false;
+    this.$inputAdress = false;
+    this.$inputEmail = false;
+    this.$inputCardNumber = false;
+    this.$inputCardValid = false;
+    this.$inputCardCVV = false;
   }
 
   init() {
@@ -29,6 +45,16 @@ export default class ModalPayment extends Component {
     }
     this.$btnSubmit = this.$root.find(`#btn-submit-buy`);
     this.$btnClose = this.$root.find(`#btn-close-modal`);
+
+    this.$modalForm = this.$root.find(`.modal-form`);
+    this.$inputName = this.$root.find(`#input-modal-name`);
+    this.$inputNumber = this.$root.find(`#input-modal-number`);
+    this.$inputAdress = this.$root.find(`#input-modal-adress`);
+    this.$inputEmail = this.$root.find(`#input-modal-email`);
+    this.$inputCardNumber = this.$root.find(`#input-card-number`);
+    this.$inputCardValid = this.$root.find(`#input-card-valid`);
+    this.$inputCardCVV = this.$root.find(`#input-card-cvv`);
+
     this.subscribe(Actions.PRODUCT_PAGE_OPEN_MODAL, () => {
       this.$root.removeClass('hidden');
     });
@@ -43,7 +69,42 @@ export default class ModalPayment extends Component {
         this.$root.addClass('hidden');
       }
       if (isBtnSubmit) {
-        //TODO
+        if (this.$inputName && this.$inputNumber && this.$inputAdress && this.$inputEmail && this.$inputCardNumber && this.$inputCardValid && this.$inputCardCVV) {
+          if (validName(this.$inputName.text() as string) === false) {
+            this.$inputName.addClass('is-invalid');
+          }
+          if (validPhone(this.$inputNumber.text() as string) === false) {
+            this.$inputNumber.addClass('is-invalid');
+          }
+          if (validAddress(this.$inputAdress.text() as string) === false) {
+            this.$inputAdress.addClass('is-invalid');
+          }
+          if (validEmail(this.$inputEmail.text() as string) === false) {
+            this.$inputEmail.addClass('is-invalid');
+          }
+          if (validEmail(this.$inputEmail.text() as string) === false) {
+            this.$inputEmail.addClass('is-invalid');
+          }
+          if (validCard(this.$inputCardNumber.text() as string) === false) {
+            this.$inputCardNumber.addClass('is-invalid');
+          }
+          if (validCardDate(this.$inputCardValid.text() as string) === false) {
+            this.$inputCardValid.addClass('is-invalid');
+          }
+          if (validCVV(this.$inputCardCVV.text() as string) === false) {
+            this.$inputCardCVV.addClass('is-invalid');
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit(e: Event) {
+    const $target = wrapperNode(e.target as HTMLElement);
+    if (this.$btnSubmit) {
+      const isBtnSubmit = $target.attr('id') === this.$btnSubmit.attr('id');
+      if (isBtnSubmit) {
+        console.log('isBtnSubmit')
       }
     }
   }
